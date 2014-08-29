8cd6b952252cf2be3280c462eb71d5bb75403ef5
Luciano Correia <luciano.correia@gmail.com>
2014-08-29 17:12:33 -0300

    start

---
+++ Correia.Rural/app/config.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+(function () {    
+
+    var ApplicationConfiguration = (function () {
+        var applicationModuleName = 'Correia.Rural';
+        var applicationModuleVendorDependencies = [''];
+    })();
+
+})();
