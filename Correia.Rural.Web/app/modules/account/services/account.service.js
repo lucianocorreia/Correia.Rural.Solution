625f727124c73415aa24877641ec3d5863e9de04
Luciano Correia <luciano.correia@gmail.com>
2014-09-04 17:37:59 -0300

    account service

--- Correia.Rural.Web/app/modules/account/services/account.service.js
+++ Correia.Rural.Web/app/modules/account/services/account.service.js
@@ -2,22 +2,31 @@
 
 (function () {
 
-	angular.module('account').factory('accountService', authenticationService);
+    angular.module('account').factory('accountService', authenticationService);
 
-	authenticationService.$inject = ['$http', '$q', 'localStorageService'];
+    authenticationService.$inject = ['$http', '$q', 'localStorageService'];
 
-	function authenticationService($http, $q, localStorageService) {
-        
-	    var _apiBase = ApplicationConfiguration.ServiceBaseUrl;
-	    var _factoryObj = {};
+    function authenticationService($http, $q, localStorageService) {
 
-	    var _authentication = {
-	        isAuthenticated: false,
-	        userName: ""
-	    };
+        var _apiBase = ApplicationConfiguration.ServiceBaseUrl;
+        var _factoryObj = {};
+
+        var _authentication = {
+            isAuthenticated: false,
+            userName: ""
+        };
 
 
+        /**
+         * Salvar dados do usuário
+         */
+        var _register = function (accountData) {
+            _logout();
+            return $http.post(_apiBase + 'account/register', accountData).then(function (response) {
+                return response;
+            });
+        };
 
-	};
+    };
 
 })();
